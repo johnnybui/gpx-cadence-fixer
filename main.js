17df456f5bdbe629afc4fe169681153f6a6d9b66
Johnny Bui <phuc.m.bui@gmail.com>
2022-05-18 06:54:52 +0700

    update

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const { XMLParser, XMLBuilder } = require('fast-xml-parser');
 
 // Read the file
-const xml = fs.readFileSync('Son_Tra_Cycling.gpx', 'utf8');
+const fileName = '20220518NightCycling.gpx';
+const xml = fs.readFileSync(`./data/${fileName}`, 'utf8');
 
 // Parse the XML
 const options = {
@@ -14,7 +15,7 @@ const jObj = parser.parse(xml);
 // console.log(JSON.stringify(jObj, null, 2).substring(0, 1000));
 
 // Fix data
-const matchedDataSet = jObj.gpx.trk.trkseg.trkpt.filter(i => i.extensions['gpxtpx:TrackPointExtension']['gpxtpx:cad'] > 82);
+const matchedDataSet = jObj.gpx.trk.trkseg.trkpt.filter(i => i.extensions['gpxtpx:TrackPointExtension']['gpxtpx:cad'] > 90);
 for (const item of matchedDataSet) {
   item.extensions['gpxtpx:TrackPointExtension']['gpxtpx:cad'] /= 2;
 }
@@ -25,4 +26,4 @@ const builder = new XMLBuilder(options);
 const xmlContent = builder.build(jObj);
 
 // Write to file
-fs.writeFileSync('Son_Tra_Cycling_Fixed.gpx', xmlContent);
+fs.writeFileSync(`./fixed-data/${fileName}`, xmlContent);
